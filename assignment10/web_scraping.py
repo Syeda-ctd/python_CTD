2dda888c5e4e8b349c8960375507ae62b5d7a24c
Syeda-ctd <syeda4u25@gmail.com>
2026-01-29 10:11:18 -0700

    first task completed

---
+++ assignment10/web_scraping.py
@@ -0,0 +1,6 @@
+# Task1
+# Review robots.txt to Ensure Policy Compliance
+# The robots.txt file for https://durhamcountylibrary.org was reviewed.
+# It disallows scraping of the /staff/ directory for all user agents.
+# This scraping assignment only accesses publicly available pages
+# and avoids the restricted /staff/ section, so it complies with the site policy.
